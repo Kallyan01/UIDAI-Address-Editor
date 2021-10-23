55df7cb079e64b172dd1091a0043d74e54e7f4c2
Sayak Bhattacharjee <sayak718.s@gmail.com>
2021-10-23 23:26:20 +0530

    added animation on mouseover

---
+++ hover.js
@@ -0,0 +1,36 @@
+let e=document.getElementsByTagName("button");
+// function cc(){
+    // e.style.backgroundColor="red";
+// };
+// for(var i=1;i<3;i++){
+    //var A=[A,B]
+//for(var i=0;i<2;i++){
+A.addEventListener("mouseover",function()
+{
+    e[0].style.background="red";
+});
+
+// B.addEventListener("mouseover",function()
+// {
+    // e[1].style.background="red";
+// });
+A.addEventListener("mouseout",function()
+{
+    e[0].style.background="blue";
+});
+B.addEventListener("mouseover",function()
+ {
+    e[1].style.background="red";
+});
+B.addEventListener("mouseout",function()
+{
+    e[1].style.background="blue";
+});
+z.addEventListener("mouseover",function()
+ {
+    e[0].style.background="red";
+});
+z.addEventListener("mouseout",function()
+{
+    e[0].style.background="blue";
+});
